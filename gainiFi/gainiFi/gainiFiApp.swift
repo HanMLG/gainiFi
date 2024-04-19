cfac093e42195aabf0e2894e603c3789bda42984
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2024-04-19 16:11:44 -0300

    empty ios project

---
+++ gainiFi/gainiFi/gainiFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gainiFiApp.swift
+//  gainiFi
+//
+//  Created by Anderson Sprenger on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct gainiFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
